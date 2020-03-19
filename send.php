244db22cd6f6f092bd3aa48d7968eb8d72e4f564
1verapoles <47753672+1verapoles@users.noreply.github.com>
2020-03-19 16:10:47 +0200

    Add file

---
+++ send.php
@@ -0,0 +1,40 @@
+﻿<?php 
+if (isset($_POST['form__btn'])) {
+
+if (isset($_POST['inner__select'])) {
+  $inner__select = filter_var($_POST['inner__select'], FILTER_SANITIZE_STRING); 
+  }
+
+  if (isset($_POST['section2__input'])) {
+  $section2__input = filter_var($_POST['section2__input'], FILTER_SANITIZE_STRING); 
+  } 
+
+  if (isset($_POST['section4__input'])) {
+  $section4__input = filter_var($_POST['section4__input'], FILTER_SANITIZE_STRING); 
+  } 
+
+ if (isset($_POST['sect3r'])) {
+  $sect3r = filter_var($_POST['sect3r'], FILTER_SANITIZE_STRING); 
+  } 
+  
+if (isset($_POST['sect5r'])) {
+  $sect5r = filter_var($_POST['sect5r'], FILTER_SANITIZE_STRING); 
+  } 
+
+ if (isset($_POST['sect6r'])) {
+  $sect6r = filter_var($_POST['sect6r'], FILTER_SANITIZE_STRING); 
+  } 
+
+  if (isset($_POST['sect7r'])) {
+  $sect7r = filter_var($_POST['sect7r'], FILTER_SANITIZE_STRING); 
+  }
+
+   if (isset($_POST['name1'])) {
+  $name1 = filter_var($_POST['name1'], FILTER_SANITIZE_STRING); 
+  }
+
+  if (isset($_POST['phone1'])) {
+  $name1 = filter_var($_POST['phone1'], FILTER_SANITIZE_STRING); 
+  } 
+}
+?>
